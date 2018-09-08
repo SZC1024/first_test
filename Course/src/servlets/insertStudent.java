33a2ed590fcb139ef76f134b2397827bb22bcac1
妖精么么 <1054550112@qq.com>
2018-09-08 17:47:30 +0800

    Update insertStudent.java

--- Course/src/servlets/insertStudent.java
+++ Course/src/servlets/insertStudent.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class insertStudent extends HttpServlet{
-	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		req.setCharacterEncoding("utf-8");
